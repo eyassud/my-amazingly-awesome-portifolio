616bc34f3bee2f63ed87e7fa80f34a3f96a262b5
daniel eyassu <danieleyassu@hotmail.com>
2024-12-29 15:18:29 -0500

    Header component completed.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+  let { children } = $props();
+
+  let y: number;
+  let innerHeight = 0;
+  let innerWidth = 0;
+
+  function goTop() {
+    document.body.scrollIntoView();
+  }
 </script>
 
-{@render children()}
+<div
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <div
+    class={`fixed bottom-0 w-full duration-200 flex p-10 z-[10] ` +
+      `y > 0 ? ' opacity-full pointer-events-auto' : ' pointer-events-none opacity-0'`}
+  >
+    <button
+      aria-label="up"
+      onclick={goTop}
+      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+    >
+      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
+    </button>
+  </div>
+  <Header {y}/>
+  {@render children()}
+  <Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
