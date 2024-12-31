50b58ca223e845d94be418b593259c74a154e928
daniel eyassu <danieleyassu@hotmail.com>
2024-12-31 15:51:33 -0500

    Projects section completed

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,4 +1,11 @@
-<script>
+<script lang="ts">
+  import Step from "./Step.svelte";
+
+  let steps: any[] = [
+    { name: "Smoljames Store", icon: "fa-solid fa-cart-shopping" },
+    { name: "Ultimate Todos", icon: "fa-solid fa-list-check" },
+    { name: "Pokedex", icon: "fa-solid fa-diagram-project" },
+  ];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -30,7 +37,59 @@
       >
     </div>
     <div class="relative shadow-2xl grid place-items-center">
-        <img src={"images/me.jpg"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]" />
+      <img
+        src={"images/me.jpg"}
+        alt="Dan Eyassu"
+        class="object-cover z-[2] max-h-[70vh]"
+      />
+    </div>
+  </section>
+
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col text-center">
+      <h6 class="text-lg sm:text-xl md:text-5xl">
+        A few of my creative endeavors.
+      </h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        Curious to <span class="poppins text-violet-400">see</span> my work
+      </h3>
+    </div>
+    <a
+      href="https://www.youtube.com/watch?v=-2UjwQzxvBQ"
+      target="_blank"
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+    >
+      <i class="fa-regular fa-circle-play"></i>
+      <p>Watch the video</p>
+    </a>
+    <div>
+      <Step step={steps[0]}>
+        <p>
+          Smoljames Store is a merchandsing store created with <strong
+            class="text-violet-400"
+            >Next.js Commerces.js Stripe & Node.js + Express.js!</strong
+          >Commerce.js is a product CMS and Stripe is used for all transaction
+          handling.
+        </p>
+      </Step>
+      <Step step={steps[1]}>
+        <p>
+          Ultimate Todos is a merchandsing store created with <strong
+            class="text-violet-400"
+            >Next.js Commerces.js Stripe & Node.js + Express.js!</strong
+          >Commerce.js is a product CMS and Stripe is used for all transaction
+          handling.
+        </p>
+      </Step>
+      <Step step={steps[2]}>
+        <p>
+          The Pokedex is a merchandsing store created with <strong
+            class="text-violet-400"
+            >Next.js Commerces.js Stripe & Node.js + Express.js!</strong
+          >Commerce.js is a product CMS and Stripe is used for all transaction
+          handling.
+        </p>
+      </Step>
     </div>
   </section>
 </main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import Header from "../components/Header.svelte";
   let { children } = $props();
 
-  let y: number;
+  let y: number = 1;
   let innerHeight = 0;
   let innerWidth = 0;
 
@@ -14,23 +14,23 @@
 </script>
 
 <div
-  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+    class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
-  <div
-    class={`fixed bottom-0 w-full duration-200 flex p-10 z-[10] ` +
-      `y > 0 ? ' opacity-full pointer-events-auto' : ' pointer-events-none opacity-0'`}
-  >
-    <button
-      aria-label="up"
-      onclick={goTop}
-      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+    <div
+        class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+            (y > 0
+                ? " opacity-full pointer-events-auto"
+                : " pointer-events-none opacity-0")}
     >
-      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
-    </button>
-  </div>
-  <Header {y} />
-  {@render children()}
-  <Footer />
+        <button aria-label="Go to top"
+            onclick={goTop}
+            class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+        >
+            <i class="fa-solid fa-arrow-up">
+        </button>
+    </div>
+    <Header {y} />
+    {@render children()}
+    <Footer />
 </div>
-
 <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
